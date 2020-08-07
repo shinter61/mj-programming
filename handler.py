f84df5dae76ca03cf3d6e87c1eb900426ce64a5e
shinter61 <shinter61@gmail.com>
2020-08-08 00:31:09 +0900

    Implement lipai

---
+++ handler.py
@@ -0,0 +1,19 @@
+input_data = [
+    '4m',
+    '1s',
+    '1s',
+    'haku',
+    '2m',
+    'haku',
+    '2m',
+    '1s',
+    '2m',
+    '3m',
+    '4m',
+    'haku',
+    '4m'
+]
+
+# リーパイ
+input_data.sort()
+print(input_data)
